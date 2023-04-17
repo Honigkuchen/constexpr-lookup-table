2e427ffdfa95b417bde868111861eb49bba071d7
Jonas 'HAUBOJO' Haubold <jonashaubold@live.de>
2023-04-17 23:29:28 +0200

    Swap unsigned char to std byte

--- src/main.cc
+++ src/main.cc
@@ -1,23 +1,28 @@
 // STL includes
+#include <cstddef>
 #include <iostream>
 
 // Project includes
 #include <constexpr-lookup-table.h>
 
+/// @brief This function creates a lookup table for an image. Each pixel (x,y) is mapped to a respective RGB value (r,g,b).
+/// @tparam width Width of the image
+/// @tparam height Height of the image
+/// @return Returns the lookup table
 template <unsigned int width, unsigned int height>
-[[nodiscard]] constexpr auto create_LUT()
+[[nodiscard]] consteval auto create_image_LUT()
 {
   using KeyType = std::tuple<unsigned int, unsigned int>;
-  using ValueType = std::tuple<unsigned char, unsigned char, unsigned char>;
+  using ValueType = std::tuple<std::byte, std::byte, std::byte>;
   constexpr_lookup_table::LUT<KeyType, ValueType, width * height> lut;
-  lut.add(KeyType{20, 10}, ValueType({254, 255, 253}));
+  lut.add(KeyType{20, 10}, ValueType{std::byte{254}, std::byte{255}, std::byte{253}});
   return lut;
 }
 
 int main()
 {
-  static constexpr auto lut_40_20 = create_LUT<40, 20>();
-  static constexpr unsigned char result = std::get<0>(lut_40_20(20, 10));
-  std::cout << "Value should be 254: " << static_cast<unsigned int>(result) << std::endl;
+  static constexpr auto lut_40_20 = create_image_LUT<40, 20>();
+  static constexpr std::byte pixel_20_10 = std::get<0>(lut_40_20(20, 10));
+  std::cout << "RGB value should be 254: " << std::to_integer<unsigned int>(pixel_20_10) << std::endl;
   return EXIT_SUCCESS;
 }
--- test/constexpr-lookup-table-test.cc
+++ test/constexpr-lookup-table-test.cc
@@ -10,16 +10,42 @@ using PixelCoordinateType = std::tuple<unsigned int, unsigned int>;
 using PixelValueType = std::tuple<unsigned char, unsigned char, unsigned char>;
 
 template <unsigned int width, unsigned int height>
-[[nodiscard]] constexpr auto create_LUT()
+[[nodiscard]] consteval auto create_image_LUT()
 {
   constexpr_lookup_table::LUT<PixelCoordinateType, PixelValueType, width * height> lut;
   lut.add(PixelCoordinateType{20, 10}, PixelValueType({254, 255, 253}));
   return lut;
 }
 
-TEST_F(ConstexprLookupTableTest, Test1)
+template <unsigned int width = 2, unsigned int height = 2>
+[[nodiscard]] consteval auto create_image_LUT_add_overflow()
 {
-  constexpr auto lut = create_LUT<40, 20>();
+  constexpr_lookup_table::LUT<PixelCoordinateType, PixelValueType, width * height> lut;
+  for (auto i = 0; i < 4; ++i)
+    lut.add(PixelCoordinateType{i, 10}, PixelValueType({254, 255, 253}));
+  return lut;
+}
+
+TEST_F(ConstexprLookupTableTest, Test_Add_Single_Pixel)
+{
+  constexpr auto lut = create_image_LUT<40, 20>();
+
+  constexpr auto v0 = std::get<0>(lut(20, 10));
+  constexpr auto v1 = std::get<1>(lut(20, 10));
+  constexpr auto v2 = std::get<2>(lut(20, 10));
+
+  static_assert(v0 == 254);
+  static_assert(v1 == 255);
+  static_assert(v2 == 253);
+
+  EXPECT_EQ(254, v0);
+  EXPECT_EQ(255, v1);
+  EXPECT_EQ(253, v2);
+}
+
+TEST_F(ConstexprLookupTableTest, Test_Add_Overflow)
+{
+  constexpr auto lut = create_image_LUT_add_overflow<2, 2>();
 
   constexpr auto v0 = std::get<0>(lut(20, 10));
   constexpr auto v1 = std::get<1>(lut(20, 10));
