088d31feb35b6b8263341f0a6157ad7fa62a1894
Jonas Haubold <jonashaubold@live.de>
2025-08-03 17:03:20 +0200

    Fix with defines

--- test/constexpr-lookup-table-test.cc
+++ test/constexpr-lookup-table-test.cc
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <constexpr-lookup-table/constexpr-lookup-table.hpp>
+#include <constexpr-lookup-table/defines.hpp>
 
 class ConstexprLookupTableTest : public ::testing::Test
 {
@@ -10,7 +11,7 @@ using PixelCoordinateType = std::tuple<unsigned int, unsigned int>;
 using PixelValueType = std::tuple<unsigned char, unsigned char, unsigned char>;
 
 template <unsigned int width, unsigned int height>
-[[nodiscard]] consteval auto create_image_LUT()
+CLT_NODISCARD CLT_CONSTEVAL auto create_image_LUT()
 {
   CLT::LUT<PixelCoordinateType, PixelValueType, width * height> lut(
       std::make_pair(PixelCoordinateType{20, 10}, PixelValueType{254, 255, 253}));
@@ -19,11 +20,11 @@ template <unsigned int width, unsigned int height>
 
 TEST_F(ConstexprLookupTableTest, Test_Add_Single_Pixel)
 {
-  static constexpr auto lut = create_image_LUT<40, 20>();
+  static CLT_CONSTEXPR auto lut = create_image_LUT<40, 20>();
 
-  constexpr auto v0 = std::get<0>(lut(20, 10));
-  constexpr auto v1 = std::get<1>(lut(20, 10));
-  constexpr auto v2 = std::get<2>(lut(20, 10));
+  CLT_CONSTEXPR auto v0 = std::get<0>(lut(20, 10));
+  CLT_CONSTEXPR auto v1 = std::get<1>(lut(20, 10));
+  CLT_CONSTEXPR auto v2 = std::get<2>(lut(20, 10));
 
   static_assert(v0 == 254);
   static_assert(v1 == 255);
